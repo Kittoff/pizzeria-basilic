ff411d6386a4804e27b375d77c65dd3d1dab82e4
Kittoff <lozano.christophe@protonmail.com>
2024-05-07 11:07:54 +0200

    removed header

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 // import "../global.css";
-import Header from "./components/header/index.jsx";
+// import Header from "./components/header/index.jsx";
 import SmoothScroll from "../components/SmoothScroll.jsx";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={poppins.className}>
         <div className="wrapper">
-          <Header />
+          {/* <Header /> */}
           <SmoothScroll>{children}</SmoothScroll>
         </div>
       </body>
