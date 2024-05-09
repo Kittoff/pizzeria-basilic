09c0d9755e98ba2ce4561398029b85e0dcf4ad3a
Kittoff <lozano.christophe@protonmail.com>
2024-05-09 09:50:31 +0200

    remove dark mode reviews

--- src/components/testimonials/Reviews.jsx
+++ src/components/testimonials/Reviews.jsx
@@ -4,7 +4,7 @@ const Reviews = ({ name, date, title, testimonial }) => {
   return (
     <div>
       <body class="flex flex-col justify-center text-bg items-center bg-customWhite ">
-        <div class="flex flex-col gap-2 min-h-40 max-w-md w-full bg-customWhite dark:bg-neutral-900 p-3 rounded-md mt-8 shadow-md hover:scale-105 hover:duration-150 duration-150">
+        <div class="flex flex-col gap-2 min-h-40 max-w-md w-full bg-customWhite  p-3 rounded-md mt-8 shadow-md hover:scale-105 hover:duration-150 duration-150">
           <div class="flex flex-row justify-between w-full">
             <div class="flex flex-row justify-between w-full">
               <p class="text-xs">{name}</p>
