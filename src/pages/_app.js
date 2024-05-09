4c27009664ac4510a91c8699b1762b231ac413ba
Kittoff <lozano.christophe@protonmail.com>
2024-05-09 09:28:26 +0200

    test removed desktop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -52,50 +52,7 @@ export default function App({ Component, pageProps, router }) {
           </div>
 
           {/* MENU DESKTOP */}
-          <div className="xs:hidden lg:block 2xl:block">
-            <nav className="flex items-center justify-between">
-              <div className="logo">
-                <Image
-                  src="/logo.png"
-                  alt="pizza basilic logo"
-                  width={146}
-                  height={146}
-                />
-                {/* 186 desktop */}
-              </div>
 
-              {/* DESKTOP */}
-              <div
-                className={`${poppins.className} flex content-between min-w-[47.063rem] text-[18px] justify-evenly font-bold`}
-              >
-                <Link href="/">Accueil</Link>
-                <Link href="/#pizza" onClick={() => handleClick("#pizza")}>
-                  Nos Pizzas
-                </Link>
-                <Link href="/contact">Nous trouver</Link>
-                <Link href="news">Actualités</Link>
-              </div>
-              <div className=" w-[250px] h-[70px] text-[0.625rem] flex items-center justify-center bg-primary content-center rounded-[30px]">
-                <Image
-                  className="mr-2"
-                  src="/phone.png"
-                  alt="phone icon"
-                  width={27}
-                  height={29}
-                />
-
-                <a
-                  className="text-bg text-[25px] font-medium xl:text-[25px] lg:text-[16px]"
-                  href="tel:0557545717"
-                >
-                  05 57 54 57 17
-                </a>
-              </div>
-              {/* <div className={styles.nav_burger}>
-        <Burger />
-      </div> */}
-            </nav>
-          </div>
           <SmoothScroll>
             <AnimatePresence mode="wait">
               <Component key={router.route} {...pageProps} />
