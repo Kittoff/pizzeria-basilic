2e1d8fbdc4f455025ff5fd1c20fc0622e39228ba
Kittoff <lozano.christophe@protonmail.com>
2024-05-22 15:28:16 +0200

    fixed call button + border

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,10 +6,27 @@ import Menu from "./menu";
 import { AnimatePresence } from "framer-motion";
 import Image from "next/image.js";
 import { FiPhoneCall } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 export default function Index() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [showTopBtn, setShowTopBtn] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1050) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   useEffect(() => {
     if (menuIsOpen) {
       document.body.classList.add("overflow-y-hidden");
@@ -29,8 +46,24 @@ export default function Index() {
         />
         {/* 186 desktop */}
       </div>
-      <div className="w-[112px] h-[28px] text-[0.625rem] flex items-center flex-initial bg-primary justify-center rounded-[30px] text-bg font-bold ">
-        <FiPhoneCall size={15} className="text-bg mr-2" />
+      <div
+        className={`${
+          showTopBtn && " border-2 border-bg z-[2] fixed"
+        } w-[112px] h-[28px] text-[0.625rem] flex items-center flex-initial bg-primary justify-center rounded-[30px] text-bg font-bold `}
+      >
+        <motion.div
+          animate={{
+            rotate: [0, 20, 20, 0, 0, 0, 20, 20, 0, 0],
+          }}
+          transition={{
+            // times: [0, 0.25, 0.5, 0.75, 1],
+            duration: 0.2,
+            repeat: Infinity,
+            repeatDelay: 4,
+          }}
+        >
+          <FiPhoneCall size={15} className="text-bg mr-2" />
+        </motion.div>
 
         <a href="tel:0557545717">Appelez-nous</a>
       </div>
