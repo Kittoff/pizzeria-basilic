542990da859a8062f316cf2cacb17e58e8c24863
Kittoff <lozano.christophe@protonmail.com>
2024-09-16 11:35:25 +0200

    added call us mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import BurgerHeader from "./burgerMenu/BurgerHeader";
 import DesktopMenu from "./desktopMenu/DesktopMenu";
 import Image from "next/image";
+import { FiPhoneCall } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
@@ -14,6 +16,24 @@ const Header = () => {
         width={146}
         height={146}
       />
+      <div
+        className={`lg:hidden w-[112px] h-[28px] text-[0.625rem] flex items-center flex-initial bg-primary justify-center rounded-[30px] text-bg font-bold `}
+      >
+        <motion.div
+          animate={{
+            rotate: [0, 20, 20, 0, 0, 0, 20, 20, 0, 0],
+          }}
+          transition={{
+            duration: 0.2,
+            repeat: Infinity,
+            repeatDelay: 3,
+          }}
+        >
+          <FiPhoneCall size={15} className="text-bg mr-2" />
+        </motion.div>
+
+        <a href="tel:0557545717">Appelez-nous</a>
+      </div>
       <BurgerHeader />
       <DesktopMenu />
     </div>
